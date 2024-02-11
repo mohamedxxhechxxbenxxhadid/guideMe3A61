3a5af0b84a95cd757766b9453b50e87eed39ce68
MBAFST <mohamed.benamor@etudiant-fst.utm.tn>
2024-02-11 21:08:34 +0100

    first commit

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,6 +5,8 @@ namespace App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
+// This is kernel
+// MBA
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
