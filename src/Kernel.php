3120042d975b55a524d342bb96a2fdd94e1fff1b
mohamedxxhechxxbenxxhadid <mohamedhechmi.benhadid@esprit.tn>
2024-02-12 16:07:00 +0100

    try gestion first time

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,9 +5,7 @@ namespace App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
-// MBA 2nd
-// This is kernel
-// MBA
+
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
